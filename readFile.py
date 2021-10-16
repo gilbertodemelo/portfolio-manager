55165bf53623a73339e6cc51da6b0360192ee94f
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2021-10-16 20:21:52 -0300

    added function to get all the transactions for a given symbol

--- readFile.py
+++ readFile.py
@@ -32,10 +32,35 @@ def get_transactions_structure(symbols: dict, data: list):
         trans_list = []
         for transaction in data:
             if item == transaction["SYMBOL"]:
-                trans_info = {"date":transaction["DATE"], "price":transaction["PRICE"], "quantity":transaction["QUANTITY"], "description":transaction["DESCRIPTION"], "amount":transaction["AMOUNT"]}
+                trans_info = {"date":transaction["DATE"], "price": transaction["PRICE"],
+                              "quantity": transaction["QUANTITY"], "description": transaction["DESCRIPTION"], "amount":transaction["AMOUNT"]}
                 trans_list.append(trans_info)
                 result[item] = trans_list
     return result
+
+
+# Returns a list of transactions for the symbol
+def get_transactions_for_symbol(transactions_dict: dict, symbol: str):
+    result = list()
+    for item in transactions_dict:
+        if item == symbol:
+            result.append(transactions_dict[symbol])
+    return result
+
+
+# Returns the current number of shares for the symbol
+def get_number_of_shares(transactions_list: list):
+    count = 0
+    for item in transactions_list:
+        for transaction in item:
+            if float(transaction["amount"]) != 0 & len(transaction["amount"]) > 0:
+                if float(transaction["amount"]) > 0:
+                    temp = int(transaction["quantity"] )
+                    count += temp
+                else:
+                    temp = int(transaction["quantity"] )
+                    count -= temp
+    return count
 
 
 def main():
@@ -43,7 +68,10 @@ def main():
     transactions_data = read_file('transactions.csv')
     transactions_symbols = get_list_of_symbols(transactions_data)
     result = get_transactions_structure(transactions_symbols, transactions_data)
-    pprint.pprint(result)
+    transactions_for_symbol = get_transactions_for_symbol(result, "JNJ")
+    number_of_shares = get_number_of_shares(transactions_for_symbol)
+    print(number_of_shares)
+
 
 
 # Call main function
