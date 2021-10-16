fd9b0a0bfb832fbcbe805bdc469db30b874c477a
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2021-10-16 17:54:58 -0300

    now all features of the program are separated in functions

--- readFile.py
+++ readFile.py
@@ -15,32 +15,34 @@ def read_file(file):
         print(IOError)
     return transactions_data
 
+
+# Create a dictionary containing transaction
+# symbols
 def get_list_of_symbols(transactions_list):
     transactions_symbols = dict()
-    # Create a dictionary containing transaction
-    #symbols
     for transaction in transactions_list:
         if transaction["SYMBOL"]:
             transactions_symbols[transaction["SYMBOL"]] = list()
     return transactions_symbols
-#
-
-def main():
-
-    transactions_data = read_file('transactions.csv')
-
-    transactions_symbols = get_list_of_symbols(transactions_data)
 
 
+def get_transactions_structure(symbols: dict, data: list):
     result = {}
-    for item in transactions_symbols.keys():
+    for item in symbols.keys():
         trans_list = []
-        for transaction in transactions_data:
+        for transaction in data:
             if item == transaction["SYMBOL"]:
                 trans_info = {"date":transaction["DATE"], "price":transaction["PRICE"], "quantity":transaction["QUANTITY"], "description":transaction["DESCRIPTION"], "amount":transaction["AMOUNT"]}
                 trans_list.append(trans_info)
                 result[item] = trans_list
+    return result
 
+
+def main():
+
+    transactions_data = read_file('transactions.csv')
+    transactions_symbols = get_list_of_symbols(transactions_data)
+    result = get_transactions_structure(transactions_symbols, transactions_data)
     pprint.pprint(result)
 
 
