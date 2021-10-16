0e36d524a5e4aceaddfbca85c5103af6723c4b05
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2021-10-16 17:32:23 -0300

    added functions to read the file and get list of stock symbols

--- readFile.py
+++ readFile.py
@@ -2,30 +2,44 @@ import csv
 import pprint
 
 
+# the readFile function reads a .csv file and
+# returns a list of transactions for the
+# period
+def read_file(file):
+    transactions_data = []
+    try:
+        transactions_file = open(file)
+        transactions_reader = csv.DictReader(transactions_file)
+        transactions_data = list(transactions_reader)
+    except IOError:
+        print(IOError)
+    return transactions_data
+
+def get_list_of_symbols(transactions_list):
+    transactions_symbols = dict()
+    # Create a dictionary containing transaction
+    #symbols
+    for transaction in transactions_list:
+        if transaction["SYMBOL"]:
+            transactions_symbols[transaction["SYMBOL"]] = list()
+    return transactions_symbols
+#
+
 def main():
 
-    transactionsFile = open('transactions.csv')
-    transactionsReader = csv.DictReader(transactionsFile)
-    transactionsData = list(transactionsReader)
+    transactions_data = read_file('transactions.csv')
 
-    #print(transactionsData)
+    transactions_symbols = get_list_of_symbols(transactions_data)
 
-    transactions_symbols = dict()
-    transaction_list = list()
-    # Create dictionary containing transaction symbols
-    for transaction in transactionsData:
-        if transaction["SYMBOL"]:
-            transactions_symbols[transaction["SYMBOL"]] = transaction_list.append(transaction)
 
     result = {}
     for item in transactions_symbols.keys():
         trans_list = []
-        for transaction in transactionsData:
+        for transaction in transactions_data:
             if item == transaction["SYMBOL"]:
                 trans_info = {"date":transaction["DATE"], "price":transaction["PRICE"], "quantity":transaction["QUANTITY"], "description":transaction["DESCRIPTION"], "amount":transaction["AMOUNT"]}
                 trans_list.append(trans_info)
                 result[item] = trans_list
-
 
     pprint.pprint(result)
 
