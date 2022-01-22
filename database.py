adae69ea954361757d1a8882107e59c6ce01bc3c
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2022-01-22 17:13:01 -0300

    added database.py

---
+++ database.py
@@ -0,0 +1,8 @@
+"""A database encapsulating collections of Stocks and their transactions """
+
+
+class StockDatabase:
+    """A database for stocks and their transactions.
+    A `StockDatabase` contains a collection of Stocks and a collection of
+    transactions.
+    """
